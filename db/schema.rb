97b9cfd97fdb85caa1a9d5c62f9afdea97490a19
Byron Roark <byronroark@gmail.com>
2015-11-06 15:42:49 -0500

    removed type from collections table

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104204717) do
+ActiveRecord::Schema.define(version: 20151106203855) do
 
   create_table "collections", force: :cascade do |t|
     t.string   "name"
-    t.string   "type"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
