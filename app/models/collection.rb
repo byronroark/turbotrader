439754acfd3b0e6e4da0ac2fbd2fb37498a79e5b
Byron Roark <byronroark@gmail.com>
2015-11-09 22:24:49 -0500

    added uniqueness validation for name in collection model

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,6 @@
 class Collection < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: { message: "You already own a collection by this name." }
+
   has_many :items
   belongs_to :user
 end
