fba5c030daf74610f5f349d0cd8ff7d7fa2b015d
Byron Roark <byronroark@gmail.com>
2015-11-11 10:25:38 -0500

    add price model and fixed item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,10 +4,30 @@ class Item < ActiveRecord::Base
 
   has_one :user, through: :collection
   belongs_to :collection
+  has_many :prices
 
   attachment :item_image, type: :image
 
   def self.search(search)
     where("title like ?", "%#{search}%")
+  end
+
+  def chart_json
+    prices.map { |price| [price.item_sold_at.to_i * 1000, price.price]}.to_json
+  end
+
+  def price_increase?
+    # Handle if there are no prices => return false
+
+    # Handle if there is only ONE price => return false
+
+    # OR
+
+    # There are at least two prices, so Return true if the last price (in 'prices') is more than the next to last price (in 'prices')
+    return true
+  end
+
+  def price_decrease?
+
   end
 end
