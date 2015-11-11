cfbe334e64080f9c13339821d3d9aa9f3df5a6c2
Byron Roark <byronroark@gmail.com>
2015-11-11 15:15:23 -0500

    seed data for items

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,35 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 50.times do |i|
+  Price.create(item_id: 1, price: 100 + rand(100), item_sold_at: 5.days.ago - i.days)
+end
+50.times do |i|
+  Price.create(item_id: 2, price: 100 + rand(100), item_sold_at: 5.days.ago - i.days)
+end
+50.times do |i|
   Price.create(item_id: 3, price: 100 + rand(100), item_sold_at: 5.days.ago - i.days)
+end
+50.times do |i|
+  Price.create(item_id: 4, price: 100 + rand(100), item_sold_at: 5.days.ago - i.days)
+end
+50.times do |i|
+  Price.create(item_id: 5, price: 100 + rand(100), item_sold_at: 5.days.ago - i.days)
+end
+50.times do |i|
+  Price.create(item_id: 6, price: 100 + rand(100), item_sold_at: 5.days.ago - i.days)
+end
+50.times do |i|
+  Price.create(item_id: 7, price: 100 + rand(100), item_sold_at: 5.days.ago - i.days)
+end
+50.times do |i|
+  Price.create(item_id: 8, price: 100 + rand(100), item_sold_at: 5.days.ago - i.days)
+end
+50.times do |i|
+  Price.create(item_id: 9, price: 100 + rand(100), item_sold_at: 5.days.ago - i.days)
+end
+50.times do |i|
+  Price.create(item_id: 10, price: 100 + rand(100), item_sold_at: 5.days.ago - i.days)
+end
+50.times do |i|
+  Price.create(item_id: 11, price: 100 + rand(100), item_sold_at: 5.days.ago - i.days)
 end
