50c7769fd5f982802c285d5d4fcb8f7178136e1f
Byron Roark <byronroark@gmail.com>
2015-11-12 17:15:34 -0500

    changed to find_or_create_by name in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,6 @@ class User < ActiveRecord::Base
   end
 
   def add_default_collection
-    self.collections.create!(name: 'Default')
+    self.collections.find_or_create_by(name: 'Default')
   end
 end
